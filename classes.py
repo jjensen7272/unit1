679584d841c94121049c2266b2f7d1f7f230ed38
Jacob Jensen <jacob.r.jensen@tooeleschools.org>
2019-02-01 09:52:18 -0700

    hello

---
+++ classes.py
@@ -0,0 +1,21 @@
+class Human(object):
+    def__init__(self):
+        hair_color
+        eye_color
+        height
+        weight
+        iq
+        gender
+        race
+    def introduce_self():
+        print("hello my name is ", self.name)
+    def discribe_self():
+        print("i have "+self.hair_color+" hair")
+        print("i have "+self.eye_color_+" eyes")
+        print("i am " +self.height +" tall")
+        print("i heigh " +self.weight + " lbs")
+        print("i am a "+ self.race+" " + self.gender+"with an iq of" + self.iq)
+
+jacob = Human()
+jacob.name = "Jacob"
+jacob.introduce_self()
